a0f7c866954be13c942feddb4dd8c162c72d077b
webInRoad <1040233545@qq.com>
2021-01-22 09:24:00 +0800

    提问功能开发

--- src/pages/index/addQuestion.js
+++ src/pages/index/addQuestion.js
@@ -1,21 +1,67 @@
 import React, { Component } from "react";
-import { View, Text, Input, Textarea } from "@tarojs/components";
+import Taro from "@tarojs/taro";
+import { View, Text, Input, Textarea, Button } from "@tarojs/components";
 import Dialog2 from "./dialog";
 import "./addquestion.less";
 export default class AddQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      des: "",
+    };
+  }
+  handleOk = () => {
+    const { title, des } = this.state;
+    if (!title || !des) {
+      Taro.showToast({
+        title: "标题与描述不能为空",
+        icon: "none",
+      });
+    } else {
+      this.props.receiveQuestion(this.state);
+    }
+  };
+  handleCancel = () => {
+    this.props.closeQuestion();
+  };
+  onChangeTitle = (event) => {
+    this.setState({
+      title: event.target.value,
+    });
+  };
+  onChangeDes = (event) => {
+    this.setState({
+      des: event.target.value,
+    });
+  };
   render() {
     return (
       <Dialog2>
         <View className="add-question">
           <View className="question-body">
             <Input
+              focus
+              onInput={this.onChangeTitle}
               className="question-input"
               placeholder="请输入您的问题标题"
             ></Input>
             <Textarea
+              onInput={this.onChangeDes}
               className="question-des"
               placeholder="请输入您的问题描述"
             ></Textarea>
+            <View className="question-btn-group">
+              <Button onClick={this.handleOk} className="question-btn ok">
+                确定
+              </Button>
+              <Button
+                onClick={this.handleCancel}
+                className="question-btn cancel"
+              >
+                取消
+              </Button>
+            </View>
           </View>
         </View>
       </Dialog2>
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import Taro from "@tarojs/taro";
 import { View, Text, Button } from "@tarojs/components";
 import AddQuestion from "./addQuestion";
 import "./index.less";
 
+function setStorage(key, value) {
+  let str = value;
+  if (typeof str == "object") {
+    str = JSON.stringify(str);
+  }
+  Taro.setStorageSync(key, str);
+}
+
+function getStorage(key) {
+  let str = Taro.getStorageSync(key);
+  if (!str) {
+    return [];
+  }
+  return JSON.parse(str);
+}
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionList: getStorage("question"),
+      showQuestionModal: false,
+    };
+  }
   componentWillMount() {}
 
   componentDidMount() {}
@@ -14,12 +37,57 @@ export default class Index extends Component {
 
   componentDidHide() {}
 
+  showQuestion = () => {
+    this.setState({
+      showQuestionModal: true,
+    });
+  };
+  receiveQuestion = (options) => {
+    this.setState(({ questionList }) => {
+      questionList.push(options);
+      return {
+        questionList,
+      };
+    });
+    console.info(this.state.questionList);
+    setStorage("question", this.state.questionList);
+    this.closeQuestion();
+  };
+  closeQuestion = () => {
+    this.setState({
+      showQuestionModal: false,
+    });
+  };
   render() {
+    const { showQuestionModal, questionList } = this.state;
+    console.info(questionList, "questionList");
     return (
       <View className="index">
         <View className="title">Taro问答实例</View>
-        <AddQuestion />
-        <Button className="btn">提问</Button>
+        <View className="question-list">
+          {questionList.map((item) => {
+            return (
+              <View className="question">
+                <View className="question-left">
+                  <View className="question-title">{item.title}</View>
+                  <View className="question-des">{item.des}</View>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+        {/* taro3版本在小程序并不会显示true,taro1估计会 */}
+        {!showQuestionModal || <AddQuestion />}
+        {/* 建议都用三目运算符，而不用|| */}
+        {showQuestionModal ? (
+          <AddQuestion
+            receiveQuestion={this.receiveQuestion}
+            closeQuestion={this.closeQuestion}
+          />
+        ) : null}
+        <Button className="btn" onClick={this.showQuestion}>
+          提问
+        </Button>
       </View>
     );
   }
